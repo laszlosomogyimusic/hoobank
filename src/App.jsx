492c9db8f2e7b89f309d588d5aa64f578869c50c
Laszlo Somogyi <laszlo.somogyi@gmail.com>
2023-08-04 05:57:59 +0200

    Skeleton ready

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,33 @@
 import React from 'react'
 import styles from './style'
+import { Billing, Business, CardDeal, Clients, CTA, Footer, Navbar, Stats, Testimonials, Hero } from "./components";
+
 
 const App = () => {
   return (
     <main className="bg-primary w-full overflow-hidden">
       <div className={`${styles.paddingX} ${styles.flexCenter}`}>
         <div className={`${styles.boxWidth}`}>
-          Navbar
+          <Navbar />
         </div>
       </div>
 
       <div className={`bg-primary ${styles.flexStart}`}>
         <div className={`${styles.boxWidth}`}>
-          Hero
+          <Hero />
         </div>
       </div>
 
       <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
         <div className={`${styles.boxWidth}`}>
-          Stats 
-          Business 
-          Billing 
-          CardDeal 
-          Testimonials 
-          Clients 
-          CTA 
-          Footer 
+          <Stats /> 
+          <Business /> 
+          <Billing /> 
+          <CardDeal /> 
+          <Testimonials /> 
+          <Clients /> 
+          <CTA /> 
+          <Footer /> 
         </div>
       </div>      
     </main>
